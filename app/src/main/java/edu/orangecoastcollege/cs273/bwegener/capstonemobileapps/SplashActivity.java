e5eea796800d76ea548708a0b2185d48c5a869aa
bwegener <wegener_brian@yahoo.com>
2017-11-13 16:36:36 -0800

    What I have so far

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/bwegener/capstonemobileapps/SplashActivity.java
@@ -0,0 +1,32 @@
+package edu.orangecoastcollege.cs273.bwegener.capstonemobileapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+
+                Intent homeIntent = new Intent(SplashActivity.this, homeActivity.class);
+
+                startActivity(homeIntent);
+
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
+    }
+}
